78fb3ca338761e56d1b3c81faa388d8fce84401a
Leo Ouyang <louyang2@stevens.edu>
2018-11-29 18:17:59 -0500

    IF the user fails to type in the correct password 3 times, the fourth failure will create a checkbox that ensures the user isn't a robot. The checkbox must be checked to progress.

--- javascript/register.js
+++ javascript/register.js
@@ -1,6 +1,19 @@
 function onClick(){
 var username = document.getElementById("formUsername").value;
 var password = document.getElementById("formPassword").value;
+var checkBox = document.getElementById("checkbox");
+    if (attempts >= 3 && checkBox == null)
+    {
+        checkBox = document.createElement("input");
+        checkBox.type = "checkbox";
+        checkBox.id = "checkbox";
+        checkBox.innerHTML = "I'm not a robot.";
+        checkBox.required = true;
+        var form = document.getElementsByTagName("form")[0];
+        form.appendChild(document.createElement("br"));
+        form.appendChild(checkBox);
+        form.appendChild(document.createTextNode("I'm not a robot."));
+    }
     if (mode == "logIn")
     {    
         var actualPassword = window.localStorage.getItem(username);
@@ -12,10 +25,20 @@ var password = document.getElementById("formPassword").value;
         else if (actualPassword != password)
         {
             document.getElementById("message").innerHTML = "That is the incorrect password! Try a different password or a different acount.";
+            attempts += 1;
         }
         else
         {
-            location.href = '../index.html';
+            if (checkBox == null || checkBox.checked)
+            {
+                location.href = '../index.html';
+            }
+            else
+            {
+               document.getElementById("message").innerHTML = "Please check the checkbox!";
+            }
+            
+
         }
     }
     else if (mode == "signUp")
@@ -26,7 +49,7 @@ var password = document.getElementById("formPassword").value;
 }
 
 var mode = "logIn";
+var attempts = 0;
 window.onload = () => {
-console.log(window.localStorage.getItem("m"));
 window.localStorage.setItem('myCat', 'Tom');
 };
